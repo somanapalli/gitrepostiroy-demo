db66eb292e8f0213797cdbd61959d937147372e4
unknown <rams.11mca@gmail.com>
2026-05-12 14:56:09 +0530

    customer file commited;

---
+++ src/com/ibm/bean/Customer.java
@@ -0,0 +1,29 @@
+package com.ibm.bean;
+
+public class Customer {
+	
+	private Integer customerId;
+	private String customerName;
+	private String custoemrAddress;
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustoemrAddress() {
+		return custoemrAddress;
+	}
+	public void setCustoemrAddress(String custoemrAddress) {
+		this.custoemrAddress = custoemrAddress;
+	}
+	
+	
+
+}
