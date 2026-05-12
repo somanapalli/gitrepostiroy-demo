56a95490d71617517ddb1fd98a5b7428d2ec2576
unknown <rams.11mca@gmail.com>
2026-05-12 15:09:20 +0530

    customer data commited

---
+++ src/com/ibm/bean/Customer.java
@@ -0,0 +1,22 @@
+package com.ibm.bean;
+
+public class Customer {
+
+	
+	private Integer custId;
+	private String custName;
+	public Integer getCustId() {
+		return custId;
+	}
+	public void setCustId(Integer custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	
+	
+}
